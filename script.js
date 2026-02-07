fc1695f0d178c7b1cfe6a433a26759ef7910cdf6
rahilmuliashia94-create <rahilmuliashia94@gmail.com>
2026-02-07 23:05:04 +0530

    Add scroll reveal functionality to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
